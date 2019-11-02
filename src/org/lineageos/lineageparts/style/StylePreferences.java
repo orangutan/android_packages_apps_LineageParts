415a166bfd3079bc046a268ead6e2ea4e7736598
orangutan <anonymousid@veryprivate.com>
2019-11-02 19:22:55 +0530

    set dark overlay to black
    Change-Id: I0c16ee057db7b5affc97392cb156fef029843424

--- src/org/lineageos/lineageparts/style/StylePreferences.java
+++ src/org/lineageos/lineageparts/style/StylePreferences.java
@@ -204,7 +204,7 @@ public class StylePreferences extends SettingsPreferenceFragment {
     private void setupStylePref() {
         int preference = LineageSettings.System.getInt(getContext().getContentResolver(),
                 LineageSettings.System.BERRY_GLOBAL_STYLE,
-                StyleInterface.STYLE_GLOBAL_AUTO_WALLPAPER);
+                StyleInterface.STYLE_GLOBAL_DARK);
         String handlerPackage = LineageSettings.System.getString(getContext().getContentResolver(),
                 LineageSettings.System.BERRY_MANAGED_BY_APP);
 
